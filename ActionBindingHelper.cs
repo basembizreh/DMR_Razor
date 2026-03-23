0deaf69da9d1a964259fc94ffea916a288dbe1e3
BasemBizreh <basem.bizreh@gmail.com>
2026-03-24 01:27:53 +0400

    MudExtensions Upgraded

---
+++ ActionBindingHelper.cs
@@ -0,0 +1,19 @@
+﻿using DMB.Core.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMR_Razor
+{
+    internal class ActionBindingHelper
+    {
+        public static bool IsDefined(ActionBinding? actionBinding)
+        {
+            return actionBinding != null
+                && !string.IsNullOrWhiteSpace(actionBinding.TargetTypeName)
+                && !string.IsNullOrWhiteSpace(actionBinding.ActionExpression);
+        }
+    }
+}
